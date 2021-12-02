9718b9f5d4d39bbc5c46f47b28d2468af5893145
pranay <pranaykawale500@gmail.com>
2021-12-02 05:51:10 +0530

    Added:Employee status is checked

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,11 @@
 console.log("Welcome to the Employee Wage Problems");
+
+const IS_ABSENT = 0;
+
+let empCheck = Math.floor(Math.random()*10)%2;
+
+if(empCheck == IS_ABSENT) {
+    console.log("Employee Absent");
+} else {
+    console.log("Employee Present");
+}
